b684249c8ab3c332f97d88e07cf4cc93e9223e5e
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2016-02-11 22:31:29 +0100

    Added Doc

--- src/Line.ts
+++ src/Line.ts
@@ -1,17 +1,30 @@
 ﻿"use strict"
 
+/**
+ * Represents a Line in Canvas
+ */
 class Line {
     
     public point1: Point;
     public point2: GeometricPoint;
     public color: Color;
 
+    /**
+     * Creates a Line beetween two points
+     * @param point1 The first Point
+     * @param point2 The second Point
+     * @param color The Color
+     */
     constructor(point1:Point,point2:GeometricPoint,color:Color) {
         this.point1 = point1;
         this.point2 = point2;
         this.color = color;
     }
 
+    /**
+     * Draw a Line
+     * @param context The Canvas Context
+     */
     public draw(context:CanvasRenderingContext2D):void{
         context.beginPath();
         context.moveTo(this.point1.x, this.point1.y);
@@ -21,6 +34,12 @@ class Line {
         context.stroke();
     }
 
+    /**
+     * Draws all Lines of a Point
+     * @param point The Point
+     * @param color The Color
+     * @param context The Canvas Context
+     */
     public static drawLines(point: Point, color: Color, context: CanvasRenderingContext2D): void {
         if (!point.isActive()) {
             return;
